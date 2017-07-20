9315df5f03d3ec6ebaaadedaccb8cbb314d7aa6e
Thaumy <Studio_ai@outlook.com>
2017-07-20 16:00:30 +0800

    1

--- build/js/hi.js
+++ build/js/hi.js
@@ -1,5 +1,5 @@
 ﻿var typed = new Typed('#hi', {
-    strings: ['你好，我叫^500Thaumy', '很^150高兴能够认^150识你', '我有^150一些爱好，例如...<br/>^800coding somrt', '我有一些爱好，例如...<br/>coding something', '我有一些爱好，例如写写程序', '这里是我的博客', '又一个码农的家.', '又一个码农的家..', '又一个码农的家...'],
+    strings: ['你好，我叫^500Thaumy', '很^150高兴能够认^150识你', '我有^150一些爱好，例如...<br/>^800coding somrt', '我有一些爱好，例如...<br/>coding something', '我有一些爱好，例如写写程序', '这里^150是我的博客', '又一个码农的家.', '又一个码农的家..', '又一个码农的家...'],
     typeSpeed: 100,
     backSpeed: 0,
 	smartBackspace: true,
