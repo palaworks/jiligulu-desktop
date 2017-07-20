b6516258932ad92bc2f63b7d8be7bede1abfc086
Thaumy <Studio_ai@outlook.com>
2017-07-20 15:01:36 +0800

    1

--- build/js/hi.js
+++ build/js/hi.js
@@ -1,4 +1,4 @@
-var typed = new Typed('#hi', {
+﻿var typed = new Typed('#hi', {
     strings: ['你好，我叫^400 Thaumy', '很高兴能够认识你', '我有一些爱好，例如...<br/>^800coding something', '我有一些爱好，例如写写程序', '这里是我的博客', '又一个码农的家.', '又一个码农的家..', '又一个码农的家...'],
     typeSpeed: 100,
     backSpeed: 0,
