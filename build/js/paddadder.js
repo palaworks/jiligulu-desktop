22ae06cc1da9f1ed7f9bcf49ba05af3211019d24
Thaumy <Studio_ai@outlook.com>
2017-08-24 14:20:46 +0800

    1

--- build/js/paddadder.js
+++ build/js/paddadder.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("a[rel='category tag']']").on("prepend",function(){$("a[rel='category tag']").prepend("<div class='padd'></div>");});$("a[class='comment!tag']").on("prepend",function(){$("a[class='comment!tag']").prepend("<div class='padd'></div>");});});
+$(document).ready(function(){$("a[rel='category tag']']").on("prepend",relf());$("a[class='comment!tag']").on("prepend",claf());});function relf(){$("a[rel='category tag']").prepend("<div class='padd'></div>");}function claf(){$("a[class='comment!tag']").prepend("<div class='padd'></div>");}
